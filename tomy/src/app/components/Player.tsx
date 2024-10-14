c7f9b566e3ac350395e0bfb4252f76d29f96324d
Julien Arbellini <arbellini.j@gmail.com>
2024-10-14 22:54:01 +0200

    ajustement taille

--- tomy/src/app/components/Player.tsx
+++ tomy/src/app/components/Player.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useRef, useEffect } from 'react';
 
-
 interface PlayerProps {
   className?: string;
   src?: string;
+  scale?: number; // Coefficient de mise à l'échelle
 }
 
 declare global {
@@ -13,19 +13,19 @@ declare global {
   }
 }
 
-const Player: React.FC<PlayerProps> = ({ src }) => {
+const Player: React.FC<PlayerProps> = ({ src, scale = 1 }) => {
   // États pour chaque bouton
   const [isPlaying, setIsPlaying] = useState(false);
   const [isPlayingAndDelay, setIsPlayingAndDelay] = useState(true);
   const [isMuted, setIsMuted] = useState(false);
-  const [isVolumeDown, setVolumeDown] = useState(false); // Assume 100 as max volume
-  const [isVolumeUp, setVolumeUp] = useState(false); // Assume 100 as max volume
+  const [isVolumeDown, setVolumeDown] = useState(false);
+  const [isVolumeUp, setVolumeUp] = useState(false);
   const [isRewinding, setIsRewinding] = useState(false);
   const [isForwarding, setIsForwarding] = useState(false);
-  const [isPressed, setIsPressed] = useState(false); // État pour l'appui du bouton Play
+  const [isPressed, setIsPressed] = useState(false);
   const [isHovering, setIsHovering] = useState(false);
   const playerRef = useRef<HTMLIFrameElement | null>(null);
-  const [player, setPlayer] = useState<any>(null); 
+  const [player, setPlayer] = useState<any>(null);
 
   const handlePlayClick = () => {
     if (!player) return;
@@ -42,7 +42,7 @@ const Player: React.FC<PlayerProps> = ({ src }) => {
 
   const handleStateChange = (event: any) => {
     if (event.data === window.YT.PlayerState.PLAYING) {
-      setIsPlayingAndDelay(false); // Retire l'écran noir quand la vidéo joue
+      setIsPlayingAndDelay(false);
     }
   };
 
@@ -53,7 +53,6 @@ const Player: React.FC<PlayerProps> = ({ src }) => {
     setIsHovering(false);
     setIsPressed(false);
   };
-  
 
   const handleMuteClick = () => {
     if (!player) return;
@@ -68,97 +67,83 @@ const Player: React.FC<PlayerProps> = ({ src }) => {
     });
   };
 
-  // Fonction pour baisser le volume
   const handleVolumeDownClick = () => {
     if (!player) return;
     const currentVolume = player.getVolume();
-    const newVolume = Math.max(currentVolume - 10, 0); // Réduit de 10%, min 0
+    const newVolume = Math.max(currentVolume - 10, 0);
     player.setVolume(newVolume);
     setVolumeDown(true);
     setTimeout(() => setVolumeDown(false), 200);
   };
 
-  // Fonction pour augmenter le volume
   const handleVolumeUpClick = () => {
     if (!player) return;
     const currentVolume = player.getVolume();
-    const newVolume = Math.min(currentVolume + 10, 100); // Augmente de 10%, max 100
+    const newVolume = Math.min(currentVolume + 10, 100);
     player.setVolume(newVolume);
     setVolumeUp(true);
     setTimeout(() => setVolumeUp(false), 200);
   };
 
   const handleRewindClick = () => {
-    if (!player) {
-      console.log("Player is not ready yet");
-      return;
-    }
-  
-    const currentTime = player.getCurrentTime(); // Obtenir le temps actuel
-    const newTime = currentTime - 10; // Reculer de 10 secondes
-    player.seekTo(Math.max(newTime, 0), true); // Empêcher de reculer avant 0 secondes
-  
-    setIsRewinding(!isRewinding); // Changer l'état visuel du bouton
+    if (!player) return;
+
+    const currentTime = player.getCurrentTime();
+    const newTime = currentTime - 10;
+    player.seekTo(Math.max(newTime, 0), true);
+
+    setIsRewinding(!isRewinding);
     if (!isRewinding) {
-      setIsForwarding(false); // Désactive le bouton Forward si Rewind est activé
+      setIsForwarding(false);
     }
   };
 
   const disableRewindClick = () => {
     setIsRewinding(false);
     if (!isRewinding) {
-      setIsForwarding(false); // Désactive le bouton Forward si Rewind est activé
+      setIsForwarding(false);
     }
   };
 
   const handleForwardClick = () => {
-    if (!player) {
-      console.log("Player is not ready yet");
-      return;
-    }
-  
-    const currentTime = player.getCurrentTime(); // Obtenir le temps actuel de la vidéo en secondes
-    const newTime = currentTime + 10; // Avancer de 10 secondes
-    player.seekTo(newTime, true); // Se déplacer à ce moment de la vidéo
-  
-    setIsForwarding(!isForwarding); // Changer l'état visuel du bouton
+    if (!player) return;
+
+    const currentTime = player.getCurrentTime();
+    const newTime = currentTime + 10;
+    player.seekTo(newTime, true);
+
+    setIsForwarding(!isForwarding);
     if (!isForwarding) {
-      setIsRewinding(false); // Désactive le bouton Rewind si Forward est activé
+      setIsRewinding(false);
     }
   };
-  
 
   const disableForwardClick = () => {
     setIsForwarding(false);
     if (!isForwarding) {
-      setIsRewinding(false); // Désactive le bouton Rewind si Forward est activé
+      setIsRewinding(false);
     }
   };
 
-// Charger l'API YouTube et initialiser le lecteur
-useEffect(() => {
-  // Charger l'API YouTube
-  console.log("test");
-  const tag = document.createElement('script');
-  tag.src = 'https://www.youtube.com/iframe_api';
-  const firstScriptTag = document.getElementsByTagName('script')[0];
-  if (firstScriptTag && firstScriptTag.parentNode) {
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  }
+  useEffect(() => {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    if (firstScriptTag && firstScriptTag.parentNode) {
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }
 
-  // Créer le lecteur après que l'API YouTube est chargée
-  (window as any).onYouTubeIframeAPIReady = () => {
-    const newPlayer = new (window as any).YT.Player(playerRef.current, {
-      events: {
-        onReady: (event: any) => {
-          console.log("Player is ready");
-          setPlayer(newPlayer); // Stocke l'instance du lecteur
+    window.onYouTubeIframeAPIReady = () => {
+      const newPlayer = new window.YT.Player(playerRef.current, {
+        events: {
+          onReady: (event: any) => {
+            setPlayer(newPlayer);
+          },
+          onStateChange: handleStateChange,
         },
-        onStateChange: handleStateChange, 
-      }
-    });
-  };
-}, []);
+      });
+    };
+  }, []);
 
   const playButtonClass = () => {
     if (isPlaying) {
@@ -180,15 +165,68 @@ useEffect(() => {
     }
   };
 
+  // Fonction pour appliquer l'échelle
+  const scaledValue = (value: number) => value * scale;
+
   return (
     <div className="absolute h-screen w-full flex justify-center items-center">
-      <div className="relative h-[553px] w-[653px] flex justify-center items-center ">
-        <img src="/vectors/ELEMENTS/Cadres/Cadre1.png" alt="" className="h-[590px] w-[645px]" />
-        <img src="/vectors/ELEMENTS/Cadres/EcranNoir.png" alt="" className={`absolute top-[23px] left-[26px] h-[490px] w-[602px] ${
-            isPlayingAndDelay ? 'z-20' : 'z-0'}`} />
-        <img src="/vectors/ELEMENTS/Cadres/vitre.png" alt="" className="absolute z-10 top-[50px] left-[33px] h-[440px] w-[586px] opacity-5" />
-        <div className="absolute top-[53px] left-[33px] h-[435px] w-[590px] overflow-hidden">
-          <div className="absolute top-[-653px] left-[0px] h-[435px] w-[586px]">
+      <div
+        className="relative flex justify-center items-center"
+        style={{
+          height: `${scaledValue(555)}px`,
+          width: `${scaledValue(645)}px`,
+        }}
+      >
+        <img
+          src="/vectors/ELEMENTS/Cadres/Cadre1.png"
+          alt=""
+          style={{
+            height: `${scaledValue(555)}px`,
+            width: `${scaledValue(645)}px`,
+          }}
+        />
+        <img
+          src="/vectors/ELEMENTS/Cadres/EcranNoir.png"
+          alt=""
+          className={`absolute`}
+          style={{
+            top: `${scaledValue(60)}px`,
+            left: `${scaledValue(26)}px`,
+            height: `${scaledValue(430)}px`,
+            width: `${scaledValue(602)}px`,
+            zIndex: isPlayingAndDelay ? 20 : 0,
+          }}
+        />
+        <img 
+          src="/vectors/ELEMENTS/Cadres/vitre.png"
+          alt="" 
+          className="absolute z-10 top-[50px] left-[33px] h-[440px] w-[586px] opacity-20" 
+          style={{
+            top: `${scaledValue(66)}px`,
+            left: `${scaledValue(30)}px`,
+            height: `${scaledValue(418)}px`,
+            width: `${scaledValue(593)}px`,
+          }}
+        />
+        <div
+          className="absolute overflow-hidden"
+          style={{
+            top: `${scaledValue(67)}px`,
+            left: `${scaledValue(33)}px`,
+            height: `${scaledValue(415)}px`,
+            width: `${scaledValue(590)}px`,
+          }}
+        >
+          <div
+            className="absolute"
+            
+            style={{
+              top: `${scaledValue(-653)}px`,
+              left: 0,
+              height: `${scaledValue(435)}px`,
+              width: `${scaledValue(586)}px`,
+            }}
+          >
             <iframe
               ref={playerRef}
               width="100%"
@@ -202,13 +240,25 @@ useEffect(() => {
           </div>
         </div>
 
-        
         {/* Bouton Exit */}
-        <div className="absolute top-[2px] left-[28px] h-[17px] w-[17px] bg-[url('/vectors/ELEMENTS/BoutonsPlayer/Exit.png')] hover:bg-[url('/vectors/ELEMENTS/BoutonsPlayer/ExitHover.png')] bg-cover hover:cursor-pointer"></div>
+        <div className={`absolute bg-[url('/vectors/ELEMENTS/BoutonsPlayer/Exit.png')] hover:bg-[url('/vectors/ELEMENTS/BoutonsPlayer/ExitHover.png')] bg-cover hover:cursor-pointer`}
+        style={{
+          top: `${scaledValue(20)}px`,
+          left: `${scaledValue(24)}px`,
+          height: `${scaledValue(25)}px`,
+          width: `${scaledValue(25)}px`,
+        }}
+        ></div>
 
         {/* Bouton Play */}
         <div
-          className={`absolute bottom-[9px] left-[30px] h-[27px] w-[27px] bg-cover hover:cursor-pointer ${playButtonClass()}`}
+          className={`absolute bg-cover hover:cursor-pointer ${playButtonClass()}`}
+          style={{
+            bottom: `${scaledValue(30)}px`,
+            left: `${scaledValue(30)}px`,
+            height: `${scaledValue(27)}px`,
+            width: `${scaledValue(27)}px`,
+          }}
           onClick={handlePlayClick}
           onMouseDown={handlePlayMouseDown}
           onMouseUp={handlePlayMouseUp}
@@ -216,13 +266,67 @@ useEffect(() => {
           onMouseLeave={handlePlayMouseLeave}
         ></div>
 
-        {/* Bouton Backwards */}
+        {/* Bouton Mute */}
         <div
-          className={`absolute bottom-[9px] left-[63px] h-[27px] w-[27px] bg-cover hover:cursor-pointer ${
+          className={`absolute bg-cover hover:cursor-pointer ${
+            isMuted
+              ? 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/MuteClic.png")]'
+              : 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/Mute.png")] hover:bg-[url("/vectors/ELEMENTS/BoutonsPlayer/MuteHover.png")]'
+          }`}
+          style={{
+            bottom: `${scaledValue(30)}px`,
+            right: `${scaledValue(95)}px`,
+            height: `${scaledValue(27)}px`,
+            width: `${scaledValue(27)}px`,
+          }}
+          onClick={handleMuteClick}
+        ></div>
+
+        {/* Bouton VolumeDown */}
+        <div
+          className={`absolute bg-cover hover:cursor-pointer ${
+            isVolumeDown
+              ? 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/VolumeDownClic.png")]'
+              : 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/VolumeDown.png")] hover:bg-[url("/vectors/ELEMENTS/BoutonsPlayer/VolumeDownHover.png")]'
+          }`}
+          style={{
+            bottom: `${scaledValue(30)}px`,
+            right: `${scaledValue(62)}px`,
+            height: `${scaledValue(27)}px`,
+            width: `${scaledValue(27)}px`,
+          }}
+          onClick={handleVolumeDownClick}
+        ></div>
+
+        {/* Bouton VolumeUp */}
+        <div
+          className={`absolute bg-cover hover:cursor-pointer ${
+            isVolumeUp
+              ? 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/VolumeUpClic.png")]'
+              : 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/VolumeUp.png")] hover:bg-[url("/vectors/ELEMENTS/BoutonsPlayer/VolumeUpHover.png")]'
+          }`}
+          style={{
+            bottom: `${scaledValue(30)}px`,
+            right: `${scaledValue(29)}px`,
+            height: `${scaledValue(27)}px`,
+            width: `${scaledValue(27)}px`,
+          }}
+          onClick={handleVolumeUpClick}
+        ></div>
+
+        {/* Bouton Rewind */}
+        <div
+          className={`absolute bg-cover hover:cursor-pointer ${
             isRewinding
               ? 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/BackwardsClic.png")]'
               : 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/Backwards.png")] hover:bg-[url("/vectors/ELEMENTS/BoutonsPlayer/BackwardsHover.png")]'
           }`}
+          style={{
+            bottom: `${scaledValue(30)}px`,
+            left: `${scaledValue(63)}px`,
+            height: `${scaledValue(27)}px`,
+            width: `${scaledValue(27)}px`,
+          }}
           onMouseDown={handleRewindClick}
           onMouseUp={disableRewindClick}
           onMouseLeave={disableRewindClick}
@@ -230,45 +334,20 @@ useEffect(() => {
 
         {/* Bouton Forward */}
         <div
-          className={`absolute bottom-[9px] left-[96px] h-[27px] w-[27px] bg-cover hover:cursor-pointer ${
+          className={`absolute bg-cover hover:cursor-pointer ${
             isForwarding
               ? 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/ForwardClic.png")]'
               : 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/Forward.png")] hover:bg-[url("/vectors/ELEMENTS/BoutonsPlayer/ForwardHover.png")]'
           }`}
-            onMouseDown={handleForwardClick}
-            onMouseUp={disableForwardClick}
-            onMouseLeave={disableForwardClick}
-
-        ></div>
-
-        {/* Bouton Mute */}
-        <div
-          className={`absolute bottom-[9px] right-[95px] h-[27px] w-[27px] bg-cover hover:cursor-pointer ${
-            isMuted
-              ? 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/MuteClic.png")]'
-              : 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/Mute.png")] hover:bg-[url("/vectors/ELEMENTS/BoutonsPlayer/MuteHover.png")]'
-          }`}
-          onClick={handleMuteClick}
-        ></div>
-
-        {/* Bouton VolumeDown */}
-        <div
-          className={`absolute bottom-[9px] right-[62px] h-[27px] w-[27px] bg-cover hover:cursor-pointer ${
-            isVolumeDown
-              ? 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/VolumeDownClic.png")]'
-              : 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/VolumeDown.png")] hover:bg-[url("/vectors/ELEMENTS/BoutonsPlayer/VolumeDownHover.png")]'
-          }`}
-          onClick={handleVolumeDownClick}
-        ></div>
-
-        {/* Bouton VolumeUp */}
-        <div
-          className={`absolute bottom-[9px] right-[29px] h-[27px] w-[27px] bg-cover hover:cursor-pointer ${
-            isVolumeUp
-              ? 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/VolumeUpClic.png")]'
-              : 'bg-[url("/vectors/ELEMENTS/BoutonsPlayer/VolumeUp.png")] hover:bg-[url("/vectors/ELEMENTS/BoutonsPlayer/VolumeUpHover.png")]'
-          }`}
-          onClick={handleVolumeUpClick}
+          style={{
+            bottom: `${scaledValue(30)}px`,
+            left: `${scaledValue(96)}px`,
+            height: `${scaledValue(27)}px`,
+            width: `${scaledValue(27)}px`,
+          }}
+          onMouseDown={handleForwardClick}
+          onMouseUp={disableForwardClick}
+          onMouseLeave={disableForwardClick}
         ></div>
       </div>
     </div>
