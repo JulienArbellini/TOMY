cf19b5a09be89ac25047cf22c8ef519176b767f7
Julien Arbellini <arbellini.j@gmail.com>
2024-10-27 19:40:31 +0100

    ombres rectif erreurs

--- tomy/src/app/components/Three/PlaneModel.tsx
+++ tomy/src/app/components/Three/PlaneModel.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { useGLTF } from '@react-three/drei';
+import * as THREE from 'three';
 
+
+interface AirplaneProps {
+    castShadow?: boolean;
+    receiveShadow?: boolean;
+  }
 // Remplace le chemin du fichier par celui de ton modèle dans le dossier public
-const PlaneModel = () => {
+const PlaneModel: React.FC<AirplaneProps> = ({ castShadow, receiveShadow }) => {
   const { scene } = useGLTF('/three/logo.glb'); // chemin du modèle
-  
+  const material = new THREE.MeshPhysicalMaterial({
+    color: 0xcccccc,
+    metalness: 0.7,
+    roughness: 0.3,
+    clearcoat: 0.5,
+    clearcoatRoughness: 0.1,
+  });
 
-  return <primitive object={scene} scale={20} />;
+  return (
+    <mesh material={material}>
+        <primitive object={scene} scale={20} />;
+  </mesh>
+  )
 };
 
 export default PlaneModel;
--- tomy/src/app/components/Three/ThreeScene.tsx
+++ tomy/src/app/components/Three/ThreeScene.tsx
@@ -1,12 +1,13 @@
 import React, { useRef, useEffect } from 'react';
-import { Canvas, useThree } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Environment } from '@react-three/drei';
 import * as THREE from 'three';
+import { Mesh } from 'three';
 import Airplane from './PlaneModel';
 
 const Scene = () => {
-  const directionalLightRef = useRef();
-  const targetRef = useRef();
+  const directionalLightRef = useRef<THREE.DirectionalLight>(null);
+  const targetRef = useRef<Mesh>(null);  // Typage explicite pour un Mesh
 
   useEffect(() => {
     if (directionalLightRef.current && targetRef.current) {
@@ -15,18 +16,29 @@ const Scene = () => {
   }, []);
 
   return (
-    <Canvas camera={{ position: [10, 1, 5], fov: 75 }}>
-      {/* Lumières */}
+    <Canvas shadows camera={{ position: [10, 1, 5], fov: 75 }}>
+      {/* Lumières supplémentaires */}
       <ambientLight intensity={1} color={0xffffff} />
       <directionalLight
         ref={directionalLightRef}
         position={[10, 10, 10]}
-        intensity={100}
+        intensity={1}
+        castShadow
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
       />
-      <object3D ref={targetRef} position={[0, 0, 0]} />
 
-      {/* Modèle */}
-      <Airplane />
+      {/* Cible de la lumière directionnelle */}
+      <mesh ref={targetRef} position={[0, 0, 0]}>
+        <sphereGeometry args={[0.1, 16, 16]} />
+        <meshBasicMaterial color="red" />
+      </mesh>
+
+      {/* Environnement HDRI */}
+      <Environment preset="sunset" />
+
+      {/* Modèle d'avion */}
+      <Airplane castShadow receiveShadow />
 
       {/* Contrôles de caméra */}
       <OrbitControls />
