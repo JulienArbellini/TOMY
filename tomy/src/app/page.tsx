fdedb2b9ba7b1166aac6b686e2eca63297c62436
Julien Arbellini <arbellini.j@gmail.com>
2025-02-17 19:10:21 +0100

    ciel partout

--- tomy/src/app/page.tsx
+++ tomy/src/app/page.tsx
@@ -132,7 +132,7 @@ export default function Home() {
     muted
     playsInline
   >
-    <source src="/vectors/ELEMENTS/ciel.mp4" type="video/mp4" />
+    <source src="https://res.cloudinary.com/dm0cuvnzt/video/upload/v1739815215/ciel.mp4" type="video/mp4" />
     {/* Alternative pour d'autres formats */}
     {/* <source src="/videos/fond_avion.webm" type="video/webm" /> */}
     Votre navigateur ne supporte pas la vidéo.
